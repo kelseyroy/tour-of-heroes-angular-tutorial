45f5361e2c2ea88c497cdd5400a50ed8518f3ecf
Kelsey Roy <kroy@8thlight.com>
2023-07-27 11:55:20 -0700

    Adds click event to select a hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import { Hero } from '../hero';
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -13,4 +13,9 @@ export class HeroesComponent {
   //   name: 'Windstorm'
   // };
   heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
