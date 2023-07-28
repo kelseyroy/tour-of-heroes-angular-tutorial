8f2af8c2304af8dd8861ecf3460227a9b2f11205
Kelsey Roy <kroy@8thlight.com>
2023-07-28 10:12:47 -0700

    rxjs Observable added to hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -26,10 +26,11 @@ export class HeroesComponent {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
-    this.getHeroes();
-  }
+  this.getHeroes();
+}
 }
